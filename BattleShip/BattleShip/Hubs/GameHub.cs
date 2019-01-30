907cd42aae4eb401b0d98454e1632c928a19dc00
Vladyslav Boiko <vladboyko3112@gmail.com>
2019-01-30 18:54:24 +0200

    GameHub added.

---
+++ BattleShip/BattleShip/Hubs/GameHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BattleShip.Models;
+using Microsoft.AspNet.SignalR;
+
+namespace BattleShip.Hubs
+{
+    public class GameHub : Hub
+    {
+        private static List<User> Users = new List<User>();
+    }
+}
