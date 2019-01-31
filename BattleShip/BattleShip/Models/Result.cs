d9c279986ebb3848892904bc1379ebefb6238b94
opakhovs <opahovskiy@gmail.com>
2019-01-31 15:59:30 +0200

    Logic of battleship

---
+++ BattleShip/BattleShip/Models/Result.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BattleShip.Models
+{
+    public class Result
+    {
+        Coord[] coords;
+        int index;
+
+        public Coord[] Coords { get { return coords; } }
+
+        public Result() {
+        }
+
+        public Result InitializeSize(int size)
+        {
+            coords = new Coord[size];
+            index = 0;
+            return this;
+        }
+
+        public bool AddCoord(Coord coord)
+        {
+            if (index == coords.Length)
+                return false;
+            coords[index++] = coord;
+            return true;
+        }
+    }
+}
---
+++ BattleShip/BattleShip/Models/Ship.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BattleShip.Models
+{
+    public class Ship
+    {
+        public Coord[] Coords { get; }
+        public bool IsHorizontal { get; }
+
+        public Ship(Coord[] coords) {
+            Coords = coords;
+            IsHorizontal = HorizontalCheck();
+        }
+
+        public bool IsSunk()
+        {
+            foreach(Coord coord in Coords)
+            {
+                if (coord.CellType != FieldType.HIT)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool HorizontalCheck()
+        {
+            if (Coords.Length == 1)
+                return true;
+            if (Coords[0].Horizontal == Coords[Coords.Length - 1].Horizontal)
+                return true;
+            return false;
+        }
+    }
+}
